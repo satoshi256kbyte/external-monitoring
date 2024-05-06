013f0d8044c6dc8af81eb2d5cd9b28fa962d766d
satoshi.kaneyasu <satoshi.kaneyasu@serverworks.co.jp>
2024-05-06 16:34:27 +0000

    test

--- app/schemas/site.py
+++ app/schemas/site.py
@@ -4,6 +4,7 @@ Webサイトモデルのスキーマ
 
 import datetime
 from typing import Union
+
 from pydantic import BaseModel
 
 
@@ -19,8 +20,8 @@ class Site(BaseModel):
     url: str
     description: Union[str, None] = None
     status: Union[int, None] = None
-    created_at: datetime
-    updated_at: datetime
+    # created_at: datetime
+    # updated_at: datetime
 
     model_config = {
         "json_schema_extra": {
@@ -29,15 +30,9 @@ class Site(BaseModel):
                     "url": "https://example.com",
                     "description": "A very nice site",
                     "status": 200,
-                    "created_at": "2021-07-01T00:00:00",
-                    "updated_at": "2021-07-01T00:00:00",
+                    # "created_at": "2021-07-01T00:00:00",
+                    # "updated_at": "2021-07-01T00:00:00",
                 }
             ]
         }
     }
-
-    class Config:
-        # フィールドのカスタム設定
-        json_encoders = {
-            datetime: lambda v: v.isoformat()  # datetime 型のフィールドを ISO 8601 形式の文字列に変換
-        }
