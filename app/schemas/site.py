370ad6f95ddeef1c94ff381399283a4bcde4df66
satoshi.kaneyasu <satoshi.kaneyasu@serverworks.co.jp>
2024-05-06 16:57:15 +0000

    test

--- app/schemas/site.py
+++ app/schemas/site.py
@@ -2,22 +2,20 @@
 Webサイトモデルのスキーマ
 """
 
+from datetime import datetime
 from typing import Union
 
 from pydantic import BaseModel
 
 
 class Site(BaseModel):
-    """
-    Webサイトモデルクラス
-
-    Args:
-        BaseModel: Pydanticのベースモデルクラス
-    """
-
     id: int
     url: str
     description: Union[str, None] = None
     status: Union[int, None] = None
-    # created_at: datetime
-    # updated_at: datetime
+    created_at: datetime
+    updated_at: datetime
+
+    # 暫定対応
+    class Config:
+        arbitrary_types_allowed = True
